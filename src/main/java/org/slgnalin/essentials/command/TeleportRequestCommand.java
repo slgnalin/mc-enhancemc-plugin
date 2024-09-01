f26561970ed6029a1fe3a89063a642ad0247cfaa
Alin Salagean <slgnalindev@gmail.com>
2024-09-02 02:47:17 +0300

    Add TeleportRequestCommand

---
+++ src/main/java/org/slgnalin/essentials/command/TeleportRequestCommand.java
@@ -0,0 +1,55 @@
+package org.slgnalin.essentials.command;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slgnalin.essentials.TeleportRequestManager;
+
+import java.util.List;
+
+public class TeleportRequestCommand implements CommandExecutor, TabExecutor {
+
+    private final TeleportRequestManager manager;
+
+    public TeleportRequestCommand(TeleportRequestManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        if (!(commandSender instanceof Player sourcePlayer)) {
+            commandSender.sendMessage("This command can only be executed by a player");
+            return true;
+        }
+
+        final String targetPlayerName = args[0];
+
+        if (commandSender.getName().equals(targetPlayerName)) {
+            commandSender.sendMessage(Component.text("You can not send a teleport request to yourself", NamedTextColor.RED));
+            return true;
+        }
+
+        final Player targetPlayer = Bukkit.getServer().getPlayer(targetPlayerName);
+
+        manager.sendRequest(sourcePlayer, targetPlayer);
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return Bukkit.getOnlinePlayers()
+                .stream()
+                .map(Player::getName)
+                .toList();
+    }
+
+}

67bc30543db5030afa45452ffd6004f1e46ccd89
Alin Salagean <slgnalindev@gmail.com>
2024-09-02 02:46:39 +0300

    Configure main class

--- src/main/java/org/slgnalin/essentials/Essentials.java
+++ src/main/java/org/slgnalin/essentials/Essentials.java
@@ -1,17 +1,31 @@
 package org.slgnalin.essentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slgnalin.essentials.command.TeleportRequestCommand;
 
 public final class Essentials extends JavaPlugin {
+    private TeleportRequestManager teleportRequestManager;
+
+    @Override
+    public void onLoad() {
+        this.teleportRequestManager = new TeleportRequestManager();
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        getCommand("teleport-request").setExecutor(new TeleportRequestCommand(teleportRequestManager));
+
+        getLogger().info("Plugin initialised");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Plugin disabled");
     }
+
+    public static Essentials getInstance(){
+        return getPlugin(Essentials.class);
+    }
+
 }

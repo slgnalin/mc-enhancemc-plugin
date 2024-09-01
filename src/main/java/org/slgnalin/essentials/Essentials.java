cabc1f55d1ba3e0bced273c8d9b28df970cb1d57
Alin Salagean <slgnalindev@gmail.com>
2024-09-02 00:21:27 +0300

    Initial commit

---
+++ src/main/java/org/slgnalin/essentials/Essentials.java
@@ -0,0 +1,17 @@
+package org.slgnalin.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Essentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

c0248830c1d3d66b662fd552b1efa8c30d8e9578
Alin Salagean <slgnalindev@gmail.com>
2024-09-04 00:02:42 +0300

    Add javadoc for TeleportRequestManager

--- src/main/java/org/slgnalin/essentials/manager/TeleportRequestManager.java
+++ src/main/java/org/slgnalin/essentials/manager/TeleportRequestManager.java
@@ -2,14 +2,38 @@ package org.slgnalin.essentials.manager;
 
 import org.bukkit.entity.Player;
 
+/**
+ * Defines the methods for managing teleport requests between players
+ */
 public interface TeleportRequestManager {
 
+    /**
+     * Registers a teleport request from one player to another
+     *
+     * @param sourcePlayer the player who initiates the teleport request
+     * @param targetPlayer the player who is the target of the teleport request
+     */
     void sendRequest(Player sourcePlayer, Player targetPlayer);
 
-    void cancelRequests(Player player);
+    /**
+     * Cancels all active (outgoing) teleport requests for the specified player
+     *
+     * @param targetPlayer the player for which to cancel the requests
+     */
+    void cancelRequests(Player targetPlayer);
 
+    /**
+     * Accepts the most recent pending (incoming) teleport request for the specified target player
+     *
+     * @param targetPlayer the target player for which to accept the request
+     */
     void acceptRequest(Player targetPlayer);
 
-    void denyRequest(Player player);
+    /**
+     * Denies the most recent pending (incoming) teleport request for the specified target player
+     *
+     * @param targetPlayer the target player for which to deny the request
+     */
+    void denyRequest(Player targetPlayer);
 
 }

4c6e3fc558a7fbe12d94bbffda333d0294de8f76
Alin Salagean <slgnalindev@gmail.com>
2024-09-03 23:18:40 +0300

    Add and configure TeleportAcceptCommand

---
+++ src/main/java/org/slgnalin/essentials/util/MessageConstants.java
@@ -0,0 +1,9 @@
+package org.slgnalin.essentials.util;
+
+public class MessageConstants {
+
+    private MessageConstants() {
+        throw new IllegalArgumentException("This class cannot be instantiated");
+    }
+
+}
